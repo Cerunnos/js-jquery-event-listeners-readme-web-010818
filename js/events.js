84ac559f632e255e5622538a8f8785cbe6a6261a
Cerunnos <github email address>
2018-02-27 12:35:00 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
 
-// call functions here
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("G")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+    return
+  })
+}
+$(document).ready(function() {
+
+  // call functions here
 
 });
